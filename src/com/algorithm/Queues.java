133a1c7b0f3f9a3c40be8e91f21d0a9812bf0ff5
jinqiao <616698865@qq.com>
2018-07-04 10:43:25 +0800

    Queues&Stacks

---
+++ src/com/algorithm/Queues.java
@@ -0,0 +1,26 @@
+package com.algorithm;
+
+import java.util.Vector;
+
+public class Queues {
+	static String[] months = {"1","2","3","4","5","6","7"};
+	
+	public static void main(String[] args){
+		Vector vq = new Vector();
+		
+		for(int i = 0; i < months.length; i++){
+			vq.addElement(months[i]+" ");
+		}
+		System.out.println("Queues:"+vq);
+		
+		//while(vq != null)错误，因为vp已经实例化了，所以永远不会是非null
+		while(!vq.isEmpty()){
+			System.out.println(vq.elementAt(0));
+			   vq.removeElementAt(0);
+		}
+		vq.clear();
+		
+		
+	}
+
+}
